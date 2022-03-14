a84f653188d99a44f2d9afc75157aaf92a6cdbd8
Theodore <theodorespyropoulos@yahoo.com>
2022-03-14 23:30:29 +1030

    Bomb generation, number near calculation, lose condition
    Also includes sprite updates, improved error handling

--- Source.cpp
+++ Source.cpp
@@ -3,15 +3,14 @@
 #include <string>
 #include <cstdlib>
 #include <vector>
+#include <windows.h>
 
 using namespace std;
 
 class tile {
 public:
-	int x;
-	int y;
 	sf::Sprite sprite;
-	int numberNear;
+	int numberNear = 0; // -1 indicates this is a bomb tile
 	bool flagged = false;
 };
 
@@ -31,8 +30,14 @@ vector<int> getMouseGridPos(int mouseX, int mouseY, int rows, int cols, int tile
 	return pos;
 }
 
+bool validCoord(int x, int y, int rows, int cols)
+{
+	return (0 <= x && x < rows && 0 <= y && y < cols);
+}
+
 int main()
 {
+	// Initialisation
 	const int WIDTH = 1280;
 	const int HEIGHT = 720;
 	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Minesweeper");
@@ -43,10 +48,13 @@ int main()
 		cout << "Font loading error." << endl;
 	}
 
+	srand(time(NULL));
+
 	int mouseX = 0;
 	int mouseY = 0;
 
 	int tileSize = 32;
+	int numBombs = 60;
 	const int rows = 20;
 	const int cols = 20;
 	int totalWidth = rows * tileSize;
@@ -55,113 +63,89 @@ int main()
 	int heightOffset = (HEIGHT - totalHeight) / 2;
 	tile grid[rows][cols];
 
-	sf::Texture s_square;
-	if (!s_square.loadFromFile("sprites/square.png"))
+	sf::Texture s_square, s_bomb, s_hit, s_flag, s_0, s_1, s_2, s_3, s_4, s_5, s_6, s_7, s_8;
+	if (!s_square.loadFromFile("sprites/square.png") || !s_bomb.loadFromFile("sprites/bomb.png") || !s_hit.loadFromFile("sprites/hit.png") || !s_flag.loadFromFile("sprites/flag.png")
+		|| !s_0.loadFromFile("sprites/0.png") || !s_1.loadFromFile("sprites/1.png") || !s_2.loadFromFile("sprites/2.png") || !s_3.loadFromFile("sprites/3.png")
+		|| !s_4.loadFromFile("sprites/4.png") || !s_5.loadFromFile("sprites/5.png") || !s_6.loadFromFile("sprites/6.png") || !s_7.loadFromFile("sprites/7.png") || !s_8.loadFromFile("sprites/8.png"))
 	{
-		cout << "Failed to load texture 'square'." << endl;
-	}
-	sf::Texture s_bomb;
-	if (!s_bomb.loadFromFile("sprites/bomb.png"))
-	{
-		cout << "Failed to load texture 'bomb'." << endl;
-	}
-	sf::Texture s_flag;
-	if (!s_flag.loadFromFile("sprites/flag.png"))
-	{
-		cout << "Failed to load texture 'flag'." << endl;
-	}
-	sf::Texture s_1;
-	if (!s_1.loadFromFile("sprites/1.png"))
-	{
-		cout << "Failed to load texture '1'." << endl;
-	}
-	sf::Texture s_2;
-	if (!s_2.loadFromFile("sprites/2.png"))
-	{
-		cout << "Failed to load texture '2'." << endl;
-	}
-	sf::Texture s_3;
-	if (!s_3.loadFromFile("sprites/3.png"))
-	{
-		cout << "Failed to load texture '3'." << endl;
-	}
-	sf::Texture s_4;
-	if (!s_4.loadFromFile("sprites/4.png"))
-	{
-		cout << "Failed to load texture '4'." << endl;
-	}
-	sf::Texture s_5;
-	if (!s_5.loadFromFile("sprites/5.png"))
-	{
-		cout << "Failed to load texture '5'." << endl;
-	}
-	sf::Texture s_6;
-	if (!s_6.loadFromFile("sprites/6.png"))
-	{
-		cout << "Failed to load texture '6'." << endl;
-	}
-	sf::Texture s_7;
-	if (!s_7.loadFromFile("sprites/7.png"))
-	{
-		cout << "Failed to load texture '7'." << endl;
-	}
-	sf::Texture s_8;
-	if (!s_8.loadFromFile("sprites/8.png"))
-	{
-		cout << "Failed to load texture '8'." << endl;
+		cout << "Failed to load texture(s)." << endl;
+		MessageBoxA(NULL, "Failed to load texture(s).", "Error!", MB_ICONERROR | MB_OK);
+		exit(0);
 	}
 
-
+	// Generate grid structure
 	for (size_t i = 0; i < rows; i++)
 	{
 		for (size_t j = 0; j < cols; j++)
 		{
-			grid[i][j].x = i;
-			grid[i][j].y = j;
-
 			sf::Sprite sprite;
 			sprite.setPosition(sf::Vector2f(i * tileSize + widthOffset, j * tileSize + heightOffset));
-			switch ((rand() % 10))
+			sprite.setTexture(s_square);
+			grid[i][j].sprite = sprite;
+		}
+	}
+
+	// Generate Bombs
+	for (int i = 0; i < numBombs; i++)
+	{
+		while (true)
+		{
+			int rRow = rand() % rows;
+			int rCols = rand() % cols;
+			if (grid[rRow][rCols].numberNear != -1)
 			{
-			case 1:
-				sprite.setTexture(s_1);
-				break;
-			case 2:
-				sprite.setTexture(s_2);
-				break;
-			case 3:
-				sprite.setTexture(s_3);
-				break;
-			case 4:
-				sprite.setTexture(s_4);
-				break;
-			case 5:
-				sprite.setTexture(s_5);
-				break;
-			case 6:
-				sprite.setTexture(s_6);
-				break;
-			case 7:
-				sprite.setTexture(s_7);
-				break;
-			case 8:
-				sprite.setTexture(s_8);
-				break;
-			case 9:
-				sprite.setTexture(s_bomb);
-				break;
-			default:
-				sprite.setTexture(s_square);
+				grid[rRow][rCols].sprite.setTexture(s_bomb);
+				grid[rRow][rCols].numberNear = -1;
 				break;
 			}
-			grid[i][j].sprite = sprite;
+		}
+	}
 
-			grid[i][j].numberNear = 1;
+	// Calculate each tile's nearby bombs
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			if (grid[i][j].numberNear == -1) {
+				continue;
+			}
+			int count = 0;
+			vector<vector<int>> coords = { {i - 1, j - 1}, {i, j - 1}, {i + 1, j - 1}, {i - 1, j  }, {i + 1, j}, {i - 1, j + 1}, {i, j + 1}, {i + 1, j + 1} };
+
+			for (auto coord : coords)
+			{
+				if (validCoord(coord[0], coord[1], rows, cols))
+				{
+					count += (grid[coord[0]][coord[1]].numberNear == -1) ? 1 : 0;
+				}
+			}
+			grid[i][j].numberNear = count;
+			switch (count) {
+			case 0: grid[i][j].sprite.setTexture(s_0);
+				break;
+			case 1: grid[i][j].sprite.setTexture(s_1);
+				break;
+			case 2: grid[i][j].sprite.setTexture(s_2);
+				break;
+			case 3: grid[i][j].sprite.setTexture(s_3);
+				break;
+			case 4: grid[i][j].sprite.setTexture(s_4);
+				break;
+			case 5: grid[i][j].sprite.setTexture(s_5);
+				break;
+			case 6: grid[i][j].sprite.setTexture(s_6);
+				break;
+			case 7: grid[i][j].sprite.setTexture(s_7);
+				break;
+			case 8: grid[i][j].sprite.setTexture(s_8);
+				break;
+			}
 		}
 	}
 
 	while (window.isOpen())
 	{
+		bool gameOver = false;
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
@@ -169,9 +153,15 @@ int main()
 			{
 				if (event.mouseButton.button == sf::Mouse::Left)
 				{
-					/*cout << "the left button was pressed" << endl;
-					cout << "mouse x: " << event.mouseButton.x << endl;
-					cout << "mouse y: " << event.mouseButton.y << endl;*/
+					vector<int> pos = getMouseGridPos(mouseX, mouseY, rows, cols, tileSize, widthOffset, heightOffset);
+					if (pos != vector<int>{-1, -1})
+					{
+						if (grid[pos[0]][pos[1]].numberNear == -1 && !grid[pos[0]][pos[1]].flagged) {
+							grid[pos[0]][pos[1]].sprite.setTexture(s_hit);
+							window.draw(grid[pos[0]][pos[1]].sprite);
+							gameOver = true;
+						}
+					}
 				}
 				if (event.mouseButton.button == sf::Mouse::Right)
 				{
@@ -237,6 +227,11 @@ int main()
 		}
 
 		window.display();
+		if (gameOver)
+		{
+			MessageBoxA(NULL, "You hit a bomb!", "Game over!", MB_ICONEXCLAMATION | MB_OK);
+			window.close();
+		}
 	}
 
 	return 0;
